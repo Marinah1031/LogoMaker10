a237f754ed78abf21d860ea9b02dc7ad205c8222
Marinah1031 <marinahtam@gmail.com>
2023-08-24 20:53:02 -0700

    adding folders and creating the perameters for the logo maker

---
+++ Main/lib/cli.js
@@ -0,0 +1,57 @@
+const inquirer = require("inquirer");
+const SVG = require("./svg");
+const { Circle, Triable, Square } = require("./shapes");
+const { writeFile } = require("fs/promises");
+
+class CLI {
+    run() {
+        return inquirer
+        .promt([
+            {
+                name: "text",
+                type:"input",
+                message:
+                "Enter the text you want to add to your svg file. (Must be more than 3 characters.)",
+                validate: (text) =>
+                    text.length <= 3 ||
+                "The message must not have more than 3 characters",
+            },
+            {
+                name: "textColor",
+                type: "input",
+                message: "Enter a text color"
+            },
+            {
+                name: "shapeType",
+                type: "list",
+                message: 'Choose one of these shapes',
+                choices: ["circle", "square", "triangle"],
+            },
+            {
+                name: "shapeColor",
+                type: "input",
+                message: "Enter a shape color",
+            },
+        ])
+        .then(({ text, textColor, shapeType, shapeColor }) => {
+            let shape;
+            switch (shapeType) {
+                case "circle":
+                    shape = new Circle();
+                    break;
+
+                case "square":
+                    shape = new Square();
+                    // console.log(shape);
+                    break;
+                
+                default:
+                    shape = new Triangle();
+                    break;
+            }
+            shape.setColor(shapeColor);
+
+            const svg = new SVG();
+        })
+    }
+}
