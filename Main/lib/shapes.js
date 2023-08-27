f9f2907f006fac9dffe59cb53191836a7984fd08
Marinah1031 <marinahtam@gmail.com>
2023-08-26 17:11:29 -0700

    adding class for triangle and square with help of ask BCS

--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -1,7 +1,7 @@
 // Main class for the shapes
 class Shape {
     constructor(color) {
-        this.color= color ?? "red"; // position of shape on canvas
+        this.color = color ?? "red"; // position of shape on canvas
     }
 
     setColor(color) {
@@ -10,21 +10,35 @@ class Shape {
 }
 
 class Circle extends Shape {
-  constructor(color) {
-    super(color)
-  }
+    constructor(color) {
+        super(color)
+    }
 
-  render() {
-    return `
+    render() {
+        return `
 
         <circle cx="150" cy="100" r="80" fill="${this.color}" />
-
-        
-    `
-  }
+    `;
+    }
 }
 
+class Triangle extends Shape {
+    render() {
+        return `
+        <polygon points="150, 18, 182, 56, 182" fill=" ${this.color}"/>
+        `;
+    }
+}
+
+class Square extends Shape {
+    render() {
+        return `<rect x="90" y="40" width="120" height=120" fill="${this.color}" />
+        `;
+}
+}
 module.exports = {
     Circle,
-    Shape
+    Shape,
+    Triangle,
+    Square 
 }
