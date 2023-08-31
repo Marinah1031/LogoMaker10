4cf81789c5b97cd9988c2550db13abd95acf2e6c
Marinah1031 <marinahtam@gmail.com>
2023-08-30 20:24:13 -0700

    adding in commentary

--- Main/lib/svg.js
+++ Main/lib/svg.js
@@ -1,7 +1,11 @@
+//this class is defined that accepts the "shape" parameter. 
+//The constructor initalizes this property
 class SVG {
     constructor(shape) {
         this.shape = shape
     }
+    //By rendering, it creates the svg markup for the graphics and it is able to take in more specific
+    //details of the height, width, etc, to customize the shape.
     render() {
         return `
         <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
@@ -12,7 +16,7 @@ class SVG {
         `
     }
 
-
+//The text is set using the properties of the colors provided, and same is applied to the shape
     setText(text, textColor) {
         this.text = text
         this.textColor = textColor
@@ -22,5 +26,5 @@ class SVG {
         this.shape = shape
     }
 }
-
+//exporting the svg class by using the module.exports makes it available for the other modules to use. 
 module.exports = { SVG }
