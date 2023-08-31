4f713a57031d495f87ff74888c50f9f365e5a190
Marinah1031 <marinahtam@gmail.com>
2023-08-30 19:41:21 -0700

    condensing the information in the file to run smoother

--- Main/lib/cli.js
+++ Main/lib/cli.js
@@ -1,50 +1,47 @@
+//
 const inquirer = require("inquirer");
 const {SVG} = require("./svg");
 const { Circle, Triangle, Square } = require("./shapes");
 const { writeFile } = require("fs/promises");
 
+
 class CLI {
-    run() {
-        return inquirer
-        .prompt([
-            {
-                name: "text",
-                type:"input",
-                message:
-                "Enter the text you want to add to your svg file. (Must be more than 3 characters.)",
-                validate: (text) =>
-                    text.length <= 3 ||
-                "The message must not have more than 3 characters",
-            },
-            {
-                name: "textColor",
-                type: "input",
-                message: "Enter a text color"
-            },
-            {
-                name: "shapeType",
-                type: "list",
-                message: 'Choose one of these shapes',
-                choices: ["circle", "square", "triangle"],
-            },
-            {
-                name: "shapeColor",
-                type: "input",
-                message: "Enter a shape color",
-            },
-        ])
-        .then(({ text, textColor, shapeType, shapeColor }) => {
+    async run() {
+        try {
+            const { text, textColor, shapeType, shapeColor } = await inquirer.prompt([
+                {
+                    name: "text",
+                    type: "input",
+                    message: "Enter the text you want to add to your svg file. (Must be less than 4 characters.)",
+                    validate: (text) => text.length <= 3 || "The message must not have less than 4 characters",
+                },
+                {
+                    name: "textColor",
+                    type: "list",
+                    message: "Choose a color you want for your text",
+                    choices: ["green", "black", "red", "blue", "orange", "yellow", "purple"],
+                },
+                {
+                    name: "shapeType",
+                    type: "list",
+                    message: 'Choose one of these shapes',
+                    choices: ["circle", "square", "triangle"],
+                },
+                {
+                    name: "shapeColor",
+                    type: "input",
+                    message: "Enter a shape color",
+                },
+            ]);
+
             let shape;
             switch (shapeType) {
                 case "circle":
                     shape = new Circle();
                     break;
-
                 case "square":
                     shape = new Square();
-                    // console.log(shape);
                     break;
-                
                 default:
                     shape = new Triangle();
                     break;
@@ -54,16 +51,16 @@ class CLI {
             const svg = new SVG();
             svg.setText(text, textColor);
             svg.setShape(shape);
-            return writeFile("logo.svg", svg.render());
-        })
-        .then(() => {
+            
+            await writeFile("logo.svg", svg.render());
             console.log("Generated logo.svg");
-        })
-        .catch((error) => {
+        } catch (error) {
             console.log(error);
             console.log("Sorry, something went wrong.");
-        });
+        }
     }
 }
+
+
 
 module.exports = CLI;
