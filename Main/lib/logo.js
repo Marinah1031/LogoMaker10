1aeabbb88b02e326b21497b63711fce3d723a86a
Marinah1031 <marinahtam@gmail.com>
2023-08-30 20:19:04 -0700

    adding description in the shapes.js file

--- Main/lib/logo.js
+++ Main/lib/logo.js
@@ -1,33 +1,40 @@
-//
+//The  packages that are used within this 
 const inquirer = require("inquirer");
 const {SVG} = require("./svg");
 const { Circle, Triangle, Square } = require("./shapes");
 const { writeFile } = require("fs/promises");
 
-//
+//The code has a class named logo
 class logo {
+    //the async method called run is responsible for running the logo application
     async run() {
         try {
+            //the inquirer.prompt() is gathering imput based on the array of questions down below.
+            //The responses are going to collect information regaring the text, color, shape, and shape color.
             const { text, textColor, shapeType, shapeColor } = await inquirer.prompt([
                 {
+                    //the text in the logo
                     name: "text",
                     type: "input",
                     message: "Enter the text you want to add to your svg file. (Must be less than 4 characters.)",
                     validate: (text) => text.length <= 3 || "The message must not have less than 4 characters",
                 },
                 {
+                    //color of the text
                     name: "textColor",
                     type: "list",
                     message: "Choose a color you want for your text",
                     choices: ["green", "black", "red", "blue", "orange", "yellow", "purple"],
                 },
                 {
+                    //shape of the logo
                     name: "shapeType",
                     type: "list",
                     message: 'Choose one of these shapes',
                     choices: ["circle", "square", "triangle"],
                 },
                 {
+                    //color of the shape
                     name: "shapeColor",
                     type: "input",
                     message: "Enter a shape color",
@@ -46,14 +53,17 @@ class logo {
                     shape = new Triangle();
                     break;
             }
+            //this is where the shape of the color is set
             shape.setColor(shapeColor);
-
+            //the svg graffics are created and in the works from the package that is used.
             const svg = new SVG();
             svg.setText(text, textColor);
             svg.setShape(shape);
-            
+            //This generated the svg content to a new file named logo.svg by using the writeFile function 
             await writeFile("logo.svg", svg.render());
+            //Once your logo is generating, this console log will get passed and you will see it display 
             console.log("Generated logo.svg");
+            //error handeling, if there are any errors that occur.
         } catch (error) {
             console.log(error);
             console.log("Sorry, something went wrong.");
@@ -62,5 +72,5 @@ class logo {
 }
 
 
-
+//making the value of this variable to be able to access other modules using the require function and connects the files together. 
 module.exports = logo;
--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -3,17 +3,18 @@ class Shape {
     constructor(color) {
         this.color = color ?? "red"; // position of shape on canvas
     }
-
+// This sets the color property of the shape 
     setColor(color) {
         this.color = color;
     }
 }
-
+//Each of the classes that are set makes an extention to the shape class which inherits the properties and methods
+//that are created in it.
 class Circle extends Shape {
     constructor(color) {
         super(color)
     }
-
+//the render method returns the SVG style for that designated shape with a this.color property
     render() {
         return `
 
@@ -39,6 +40,8 @@ class Square extends Shape {
         return `<rect x="90" y="45" width="125" height="125" fill="${this.color}" />`;
 }
 }
+
+//expriting the three shapes are exported from the module.exports which makes them available for the other modules to import.
 module.exports = {
     Circle,
     Triangle,
